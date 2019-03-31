550b14d94e7ebbd606d91268dfa9f94179b8531c
Chris <23093603+ChrisBapin@users.noreply.github.com>
2019-03-31 16:23:41 +0200

    add route redirection

---
+++ src/containers/Redirection/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import axios from "axios";
+
+class Redirection extends React.Component {
+  state = {
+    data: [],
+    visit: "",
+    id: "",
+    shortUrl: "",
+  };
+
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:8080/get_url");
+    await this.setState({ data: response.data });
+
+    for (let i = 0; i < this.state.data.length; i++) {
+      if (this.props.match.params.shorturl === this.state.data[i].shortUrl) {
+        console.log(i, this.state.data[i]);
+        this.state.data[i].visit = Number(this.state.data[i].visit) + 1;
+        await this.setState({
+          visit: this.state.data[i].visit,
+        });
+        window.location.href = this.state.data[i].longUrl;
+      }
+    }
+
+    await axios.post(
+      "http://localhost:8080/update_url/" + this.props.match.params.shorturl,
+      {
+        visit: this.state.visit,
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "3rem" }}
+      >
+        {/* <div>Redirection</div> */}
+      </div>
+    );
+  }
+}
+
+export default Redirection;
